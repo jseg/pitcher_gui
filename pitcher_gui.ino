3fc1c4eceb6c33c38919fc096a6c0c0b74b87a22
John Segner <john.segner@gmail.com>
2019-01-25 22:30:34 -0500

    wifiadd

--- pitcher_gui.ino
+++ pitcher_gui.ino
@@ -39,7 +39,7 @@ const char *OTAPassword = "esp8266";
 
 const char* mdnsName = "pitcher"; // Domain name for the mDNS responder
 
-bool verbose = true;
+bool verbose = false;
 int _state = 0;
 int _hand = 0;
 int _keyed = 0;
@@ -106,6 +106,7 @@ void startWiFi() { // Start a Wi-Fi access point, and try to connect to some giv
   }
   wifiMulti.addAP("Torrid Zone", "temp_weak_passcode");   // add Wi-Fi networks you want to connect to
   wifiMulti.addAP("luvnlife16.2G", "1cor1313");
+  wifiMulti.addAP("luvnlife16", "1cor1313" )
  
   if(verbose){
     Serial.println("Connecting");
